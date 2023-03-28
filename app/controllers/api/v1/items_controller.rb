15df52401e651d060f1b10a1e5442ae2534cfb42
Harrison Ryan <hwryan12@gmail.com>
2023-03-28 14:11:05 -0500

    feature: Implement post route and controller logic for create an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,5 +5,21 @@ class Api::V1::ItemsController < ApplicationController
 
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
+  end 
+
+  def create
+    item = Item.create(item_params)
+    if item.valid?
+      render json: ItemSerializer.new(item), status: 201
+    else
+      render json: { errors: item.errors.full_messages }, status: 422
+    end
+  end
+  
+
+  private
+
+  def item_params
+    params.permit(:id, :name, :description, :unit_price, :merchant_id)
   end
 end
