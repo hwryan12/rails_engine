065c29abc88c9d818b59dea4fae5a405aef9d124
Harrison Ryan <hwryan12@gmail.com>
2023-03-28 08:06:17 -0500

    feature: Add validations and relationships to models

---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+  validates_presence_of :name, :description, :unit_price, :merchant_id 
+  belongs_to :merchant
+  has_many :invoice_items, dependent: :delete_all
+  has_many :invoices, through: :invoice_items
+end
