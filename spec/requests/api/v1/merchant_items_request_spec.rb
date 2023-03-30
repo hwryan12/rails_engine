bb3dcc17cd621428d87e1f0924c9bd493b65b77d
Harrison Ryan <hwryan12@gmail.com>
2023-03-30 17:51:05 -0500

    test: Add more tests to specs for sad paths

--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -26,5 +26,19 @@ RSpec.describe "Merchant Items API", type: :request do
         expect(item[:attributes][:unit_price]).to be_a(Float)
       end
     end
+
+
+    it "returns an error when trying to get items for a non-existent merchant ID" do
+      get api_v1_merchant_items_path(999999) #a random merchant ID
+
+      expect(response).to_not be_successful
+      expect(response).to have_http_status(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Merchant with 'id'=999999")
+    end
   end
 end
