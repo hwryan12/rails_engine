cad29f9e2f7f02364b8cfa2c094583ebe97ff355
Harrison Ryan <hwryan12@gmail.com>
2023-03-30 12:31:17 -0500

    feature: Create item_search controller and route

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::ItemSearchController < ApplicationController
   def index
-    @item = Item.partial_name_search(item_params[:name])
-    if @item
-      render json: ItemSerializer.new(@item), status: 200
+    if item_params[:name].present? && (item_params[:min_price].present? || item_params[:max_price].present?)
+      render json: { error: "Name and price parameters cannot be sent together" }, status: :bad_request
+    elsif item_params[:name].present?
+      @items = Item.partial_name_search(item_params[:name])
+      render json: ItemSerializer.new(@items), status: 200
+    elsif item_params[:min_price].present? || item_params[:max_price].present?
+      @items = Item.price_search(item_params[:min_price], item_params[:max_price])
+      render json: ItemSerializer.new(@items), status: 200
     else
       render json: { data: {} }, status: 404
     end
@@ -10,6 +15,6 @@ class Api::V1::ItemSearchController < ApplicationController
 
   private
   def item_params
-    params.permit(:name)
+    params.permit(:name, :min_price, :max_price)
   end
 end
