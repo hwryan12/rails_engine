8c1feb9e5e35cb1c1ee839cf1d5eb25766321d02
Harrison Ryan <hwryan12@gmail.com>
2023-03-30 08:24:20 -0500

    feature: Create merchant model class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+
+  def self.partial_name_search(search)
+    Merchant.where("name ILIKE ?", "%#{search}%")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe "Class Methods" do
+    describe ".partial_name_search" do
+      it "returns a list of merchants that match the search params" do
+        merchant1 = create(:merchant, name: "Bob's Burgers")
+        merchant2 = create(:merchant, name: "Burger King")
+        merchant3 = create(:merchant, name: "In-N-Out")
+
+        expect(Merchant.partial_name_search("burger")).to eq([merchant1, merchant2])
+        expect(Merchant.partial_name_search("burger")).to_not eq([merchant3])
+        
+        expect(Merchant.partial_name_search("in")).to eq([merchant2, merchant3])
+        expect(Merchant.partial_name_search("in")).to_not eq([merchant1])
+      end
+    end
+  end
 end
